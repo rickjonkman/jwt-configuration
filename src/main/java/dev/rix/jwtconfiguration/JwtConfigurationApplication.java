4713cfb09265301c05687aa60df1909575305f89
Rick Jonkman <rickjonkman@MacBook-Air-van-Rick.local>
2023-12-19 15:59:01 +0100

    start

---
+++ src/main/java/dev/rix/jwtconfiguration/JwtConfigurationApplication.java
@@ -0,0 +1,13 @@
+package dev.rix.jwtconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtConfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/rix/jwtconfiguration/JwtConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rix.jwtconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
